5a6cae0df65380b6ef0d8e7202c00317de22bed6
nirayndra <faadiajasmine@gmail.com>
2026-04-06 20:18:39 +0700

    menambah input dengan perulangan

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -18,4 +18,19 @@ struct Orang
 int main() {
     Orang mhs[3];
 
+    for (int i = 0; i <= 2; i++)
+    {
+
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs[i].nama);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+    }
 }
