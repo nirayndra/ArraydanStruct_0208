888be10a1806498ca0e92eab124b628714abbdd7
nirayndra <faadiajasmine@gmail.com>
2026-04-06 20:17:18 +0700

    membuat struct detailalamat dan orang

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;;
+};
+
+struct Orang
+{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
