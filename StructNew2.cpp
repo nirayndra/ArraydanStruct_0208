10f3e8a9fdcea3f5af4e0b83162e91179e7ff62e
nirayndra <faadiajasmine@gmail.com>
2026-04-06 20:04:49 +0700

    menambahkan deklarasi variabel dan input data

--- StructNew2.cpp
+++ StructNew2.cpp
@@ -13,3 +13,18 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs;
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+}
