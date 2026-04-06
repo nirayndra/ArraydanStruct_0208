927e116982bdbccba3dc3b90cecdf4e8b066460f
nirayndra <faadiajasmine@gmail.com>
2026-04-06 19:55:58 +0700

    menampilkan data dari struct orang

--- StrucNew.cpp
+++ StrucNew.cpp
@@ -20,4 +20,8 @@ int main() {
     cin >> mhs.umur;
     cout << endl;
 
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
