6bcf2d2549117fd0c185fd0813bb67f5e5a43ef8
nirayndra <faadiajasmine@gmail.com>
2026-04-06 20:12:13 +0700

    implementasi input

--- Getline.cpp
+++ Getline.cpp
@@ -13,3 +13,20 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs;
+    
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs.nama);
+    cout << "Alamat : ";
+    getline(cin , mhs.alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+}
